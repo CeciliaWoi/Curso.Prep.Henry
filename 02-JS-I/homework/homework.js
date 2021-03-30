87ebd1d3e86ddfc562086f0b3498ad8d53ab7d65
CeciliaWoi <cecilia@woitasen.com.ar>
2021-03-30 20:03:38 -0300

    Corecciones clase online, homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,40 +60,28 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x === y) {
-    return true;
-  }
-  return false;
+  return x === y;
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ( str1.length === str2.length ) {
-    return true;
-  }
-  return false;
+  return str1.length === str2.length;
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ( num < 90 ) {
-    return true;
-  }
-  return false;
+  return num < 90;
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if ( num > 50) {
-    return true;
-  }
-  return false;
+  return num > 50;
 }
 
 function obtenerResto(x, y) {
@@ -106,20 +94,14 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 === 0) {
-    return true;
-  }
-  return false;
+  return num % 2 === 0;
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 === 1) {
-    return true;
-  }
-  return false;
+  return num % 2 === 1;
 }
 
 function elevarAlCuadrado(num) {
@@ -211,7 +193,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-  return ((base * altura) / 2);
+  return (base * altura) / 2;
 }
 
 
@@ -228,7 +210,7 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.length >= 2) {
+  if (letra.length > 1) {
     return "Dato incorrecto";
   }
   if ( letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' ) {
